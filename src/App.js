ec96f6ec3421d43a3ca3d044b9e7a85fc0c230fd
rohanb_coditas <rohanb_coditas@gmail.com>
2021-11-19 12:26:20 +0530

    After adding basic user details and Address fields

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Grid, Typography } from "@material-ui/core";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import Textfield from "./Components/FormsUI/Textfield";
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  formWrapper: {
+    marginTop: theme.spacing(5),
+    marginBottom: theme.spacing(8),
+  },
+}));
+
+// Formik Form Initial state
+const INITIAL_FORM_STATE = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  phone: "",
+  addressLine1: "",
+  addressLine2: "",
+  city: "",
+  state: "",
+  country: "",
+};
+
+// Form Validation Schema
+const FORM_VALIDATION = Yup.object().shape({
+  firstName: Yup.string().required("Required"),
+  lastName: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid Email!").required("Required"),
+  phone: Yup.number()
+    .integer()
+    .typeError("Please enter a valid phone number")
+    .required("required"),
+  addressLine1: Yup.string().required("Required"),
+  addressLine2: Yup.string().required("Required"),
+  city: Yup.string().required("Required"),
+  state: Yup.string().required("Required"),
+  country: Yup.string().required("Required"),
+});
+
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container>
+      <Grid item xs={12}>
+        <Container maxWidth="md">
+          <div className={classes.formWrapper}>
+            <Formik
+              initialValues={{
+                ...INITIAL_FORM_STATE,
+              }}
+              validationSchema={FORM_VALIDATION}
+              onSubmit={(values) => {
+                console.log("form values >>> ", values);
+              }}
+            >
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Typography>Your Details</Typography>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="firstName" label="First Name" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="lastName" label="Last Name" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="email" label="email" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="phone" label="Phone" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography>Address</Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="addressLine1" label="Address Line 1" />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Textfield name="addressLine2" label="Address Line 2" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="city" label="City" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Textfield name="state" label="State" />
+                  </Grid>
+                  <Grid item xs={6}>
+                    {/* placeholder for country dropdown */}
+                  </Grid>
+
+                  <Grid item xs={12}>
+                    <Typography>Booking Information</Typography>
+                  </Grid>
+                </Grid>
+              </Form>
+            </Formik>
+          </div>
+        </Container>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default App;
